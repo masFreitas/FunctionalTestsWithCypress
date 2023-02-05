60d5919064f2d771688210566a790a761dbc3985
Mateus GitHub <mateusasfreitas@gmail.com>
2023-02-05 20:28:20 -0300

    Adding Locators

--- cypress/e2e/funcional.cy.js
+++ cypress/e2e/funcional.cy.js
@@ -1,35 +1,37 @@
 /// <reference types="cypress"/>
+import loc from '../support/locators'
 
 describe('Functional Tests Barriga React', () => {
     beforeEach(() => {
         cy.visit('https://barrigareact.wcaquino.me/')
 
-        cy.get('[data-test="email"]').type('mateustcteste@gmail.com')
-        cy.get('[data-test="passwd"]').type('12345')
-        cy.get('.btn').click()
-        cy.get('.toast-message')
+        cy.get(loc.LOGIN.emailField).type('mateustcteste@gmail.com')
+        cy.get(loc.LOGIN.passwordField).type('12345')
+        cy.get(loc.LOGIN.loginBtn).click()
+        cy.get(loc.MESSAGE.toastMessage)
             .should('contain', 'Bem vindo')
     })
 
     it('Create an account with success', () => {
-        cy.get('[data-test="menu-settings"]').click()
-        cy.get('[href="/contas"]').click()
-        cy.get('[data-test="nome"]').type('Conta de teste')
-        cy.get('.btn').click()
-        cy.get('.toast-message')
+        cy.get(loc.MENU.setting).click()
+        cy.get(loc.MENU.contas).click()
+        cy.get(loc.CONTAS.nomeField).type('Conta de teste')
+        cy.get(loc.CONTAS.saveBtn).click()
+        cy.get(loc.MESSAGE.toastMessage)
             .should('contain', 'Conta inserida com sucesso!')
     })
 
     it('Edit an account with success', () => {
-        cy.get('[data-test="menu-settings"]').click()
-        cy.get('[href="/contas"]').click()
-        cy.xpath("//table//td[contains(.,'Conta de teste')]/..//i[@class='far fa-edit']")
+        cy.get(loc.MENU.setting).click()
+        cy.get(loc.MENU.contas).click()
+        cy.xpath(loc.CONTAS.editAccountButton)
             .click()
-        cy.get('[data-test="nome"]')
+        cy.get(loc.CONTAS.nomeField)
             .clear()
             .type('Conta alterada')
-        cy.get('.btn').click()
-        cy.get('.toast-message')
+        cy.get(loc.CONTAS.saveBtn).click()
+        cy.get(loc.MESSAGE.toastMessage)
             .should('contain', 'Conta atualizada com sucesso!')
     })
+
 })
---
+++ cypress/support/locators.js
@@ -0,0 +1,25 @@
+const locators = {
+    LOGIN: {
+        emailField: '[data-test="email"]',
+        passwordField:  '[data-test="passwd"]',
+        loginBtn: '.btn',
+    },
+
+    MESSAGE: {
+        toastMessage: '.toast-message',
+    },
+
+    MENU: {
+        setting: '[data-test="menu-settings"]',
+        contas: '[href="/contas"]',
+    },
+
+    CONTAS: {
+        nomeField: '[data-test="nome"]',
+        saveBtn: '.btn',
+        editAccountButton: "//table//td[contains(.,'Conta de teste')]/..//i[@class='far fa-edit']",
+    }
+
+}
+
+export default locators;
