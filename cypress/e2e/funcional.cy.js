8aa302ebbd3f2a3339b5db0fcdfdf9bfca68be65
masFreitas <mateusasfreitas@gmail.com>
2023-02-06 15:12:58 -0300

    Add create an transaction with success scenario

--- cypress/e2e/funcional.cy.js
+++ cypress/e2e/funcional.cy.js
@@ -33,6 +33,18 @@ describe('Functional Tests Barriga React', () => {
             .should('contain', 'Erro: Error: Request failed with status code 400')
     })
 
+    it('Create an transaction with success', () => {
+        cy.get(loc.MENU.movimentacao).click()
+        cy.get(loc.MOVIMENTACAO.descricaoField).type('Desc')
+        cy.get(loc.MOVIMENTACAO.valorField).type('123')
+        cy.get(loc.MOVIMENTACAO.interessadoField).type('Inter')
+        cy.get(loc.MOVIMENTACAO.statusBtn).click()
+        cy.get(loc.MOVIMENTACAO.salvarBtn).click()
+        cy.get(loc.MESSAGE.toastMessage)
+            .should('contain', 'Movimentação inserida com sucesso!')
+        cy.xpath(loc.EXTRATO.buscaElemento).should('exist')
+    })
+
     after(() => {
         cy.resetApp()
     })
--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -12,13 +12,26 @@ const locators = {
     MENU: {
         setting: '[data-test="menu-settings"]',
         contas: '[href="/contas"]',
-        reset: '[href="/reset"]'
+        reset: '[href="/reset"]',
+        movimentacao: '[data-test="menu-movimentacao"]',
     },
 
     CONTAS: {
         nomeField: '[data-test="nome"]',
         saveBtn: '.btn',
         // editAccountButton: "//table//td[contains(.,'Conta de teste')]/..//i[@class='far fa-edit']",
+    },
+
+    MOVIMENTACAO: {
+        descricaoField: '[data-test="descricao"]',
+        valorField: '[data-test="valor"]',
+        interessadoField: '[data-test="envolvido"]',
+        statusBtn: '[data-test="status"]',
+        salvarBtn: '.btn-primary'
+    },
+
+    EXTRATO:{
+        buscaElemento: "//span[contains(., 'Desc')]//following-sibling::small[contains(., '123')]"
     }
 
 }
